84caeeb8269cbdcb2e25e90d5c22c7958a941e9b
Ilhammursidi <imursidi37@gmail.com>
2026-02-28 23:25:43 +0700

    Update minitask2.js

--- minitask2.js
+++ minitask2.js
@@ -1,11 +1,11 @@
 function menghitungDiskon(jumlahPembelian) {
-    if (jumlahPembelian < 500000) {
-        return jumlahPembelian;
-    } else if (jumlahPembelian < 1000000) {
-        return jumlahPembelian-(jumlahPembelian*0.05);
-    } else {
-        return jumlahPembelian-(jumlahPembelian*0.10);
-    }
+  if (jumlahPembelian < 500000) {
+    return jumlahPembelian;
+  } else if (jumlahPembelian < 1000000) {
+    return jumlahPembelian - jumlahPembelian * 0.05;
+  } else {
+    return jumlahPembelian - jumlahPembelian * 0.1;
+  }
 }
 
 console.log(menghitungDiskon(800000));
